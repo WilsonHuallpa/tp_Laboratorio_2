71a5eed21b410ddb7877d21762ee73e466828d3b
WilsonHuallpa <60585620+WilsonHuallpa@users.noreply.github.com>
2020-10-03 19:23:55 -0300

    Add files via upload

---
+++ TP-02/Entidades/Ciclomotor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Ciclomotor : Vehiculo
+    {
+
+        /// <summary>
+        /// Constructor por defecto
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="chasis"></param>
+        /// <param name="color"></param>
+        public Ciclomotor(EMarca marca, string chasis, ConsoleColor color): base(chasis,marca,color)
+        {
+        }
+
+        /// <summary>
+        /// Las motos son chicas
+        /// </summary>
+        public override ETamanio Tamanio
+        {
+            get
+            {
+                return ETamanio.Chico;
+            }
+        }
+        /// <summary>
+        /// crea un stringbuilder con todos los datos del vehiculo
+        /// </summary>
+        /// <returns>Un string con todos los datos</returns>
+        public override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("CICLOMOTOR");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine($"TAMAÑO : {this.Tamanio}");
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
