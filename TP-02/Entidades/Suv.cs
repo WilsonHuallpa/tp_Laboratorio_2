0060eb4f3f69d41bc8af1e211946ddb76c67ef42
WilsonHuallpa <60585620+WilsonHuallpa@users.noreply.github.com>
2020-10-03 20:09:40 -0300

    Add files via upload

---
+++ TP-02/Entidades/Suv.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+   
+    public class Suv : Vehiculo
+    {
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="chasis"></param>
+        /// <param name="color"></param>
+        public Suv(EMarca marca, string chasis, ConsoleColor color): base(chasis, marca, color)
+        {
+
+        }
+
+        /// <summary>
+        /// Las camionetas son grandes
+        /// </summary>
+        public override ETamanio Tamanio 
+        {
+            get
+            {
+                return ETamanio.Mediano;
+            }
+        }
+        /// <summary>
+        /// Crea un stringbuider con todos los datos del vehiculo
+        /// </summary>
+        /// <returns>string con todos los datos</returns>
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("SUV");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine($"TAMAÑO : {this.Tamanio}");
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ TP-02/Entidades/Vehiculo.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    #pragma warning disable CS0660
+    #pragma warning disable CS0661
+
+    /// <summary>
+    /// La clase Vehiculo no deberá permitir que se instancien elementos de este tipo.
+    /// </summary>
+
+    public abstract class Vehiculo
+
+    {
+        public enum EMarca
+        {
+            Chevrolet, 
+            Ford, 
+            Renault, 
+            Toyota, 
+            BMW, 
+            Honda,
+            HarleyDavidson
+        }
+
+        public enum ETamanio
+        {
+            Chico, 
+            Mediano, 
+            Grande
+        }
+
+        EMarca marca;
+        string chasis;
+        ConsoleColor color;
+        
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="chasis"></param>
+        /// <param name="marca"></param>
+        /// <param name="color"></param>
+        public Vehiculo(string chasis, EMarca marca, ConsoleColor color)
+        {
+            this.marca = marca;
+            this.chasis = chasis;
+            this.color = color;
+        }
+
+
+
+        /// <summary>
+        /// ReadOnly: Retornará el tamaño
+        /// </summary>
+        public abstract ETamanio Tamanio {get;} 
+
+        /// <summary>
+        /// Publica todos los datos del Vehiculo.
+        /// </summary>
+        /// <returns> un string con todos los datos </returns>
+        
+        public virtual string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"CHASIS: {this.chasis}");
+            sb.AppendLine($"MARCA: {this.marca.ToString()}");
+            sb.AppendLine($"COLOR: {this.color.ToString()}");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+        
+  
+        /// <summary>
+        /// Sobrecarga de operador
+        /// </summary>
+        /// <param name="p"> Metodo mostrar del vehiculo en cuestion</param>
+        public static explicit operator string (Vehiculo p)
+        {
+            return p.Mostrar();
+        }
+
+        /// <summary>
+        /// Dos vehiculos son iguales si comparten el mismo chasis
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public static bool operator == (Vehiculo v1, Vehiculo v2)
+        {
+            return (v1.chasis == v2.chasis);
+        }
+        /// <summary>
+        /// Dos vehiculos son distintos si su chasis es distinto
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns>true si comparten el mismo chasism, false caso contrario</returns>
+        public static bool operator !=(Vehiculo v1, Vehiculo v2)
+        {
+            return !(v1.chasis == v2.chasis);
+        }
+
+    }
+}
