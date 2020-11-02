8396d2f9d5a6b3f76cb675f7287d41c8e5d58385
WilsonHuallpa <60585620+WilsonHuallpa@users.noreply.github.com>
2020-11-01 23:56:31 -0300

    Add files via upload

---
+++ Huallpa.Wilson.2D.TP3/Archivos/Clases Instanciables/Profesor.cs
@@ -0,0 +1,120 @@
+﻿using Clases_Abstractas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Clases_Instanciables
+{
+    public sealed class Profesor: Universitario
+    {
+
+        #region Atributo
+        Queue<Universidad.EClases> clasesDelDia;
+        public static Random random;
+        #endregion
+
+        #region Constructores
+        /// <summary>
+        /// Constructor estatico. Inicializa el atributo de clase.
+        /// </summary>
+        static Profesor() {
+            random = new Random();
+        }
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public Profesor():base()
+        {
+
+        }
+
+        /// <summary>
+        /// Constructor parametrizado
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="nombre"></param>
+        /// <param name="apellido"></param>
+        /// <param name="dni"></param>
+        /// <param name="nacionalidad"></param>
+        public Profesor(int id, string nombre, string apellido, string dni, ENacionalidad nacionalidad): base(id, nombre, apellido, dni, nacionalidad)
+        {
+            this.clasesDelDia = new Queue<Universidad.EClases>();
+            this._randomClases();
+        }
+        #endregion
+
+    
+
+
+        protected override string MostrarDatos()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine(base.MostrarDatos());
+            sb.AppendLine(this.ParticiparEnClase());
+
+            return sb.ToString();
+        }
+
+        protected override string ParticiparEnClase()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("CLASES DEL DIA: ");
+            foreach (Universidad.EClases item in this.clasesDelDia)
+            {
+                sb.AppendLine(item.ToString());
+            }
+
+            return sb.ToString();
+        }
+
+        public override string ToString()
+        {
+            return this.MostrarDatos();
+        }
+
+        public static bool operator ==(Profesor i, Universidad.EClases clase)
+        {
+            foreach (Universidad.EClases item in i.clasesDelDia)
+            {
+                if (item == clase)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        public static bool operator !=(Profesor i, Universidad.EClases clase)
+        {
+            return !(i == clase);
+        }
+
+        private void _randomClases()
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                int opcion = random.Next(0, 3);
+
+                switch (opcion)
+                {
+                    case 0:
+                        this.clasesDelDia.Enqueue(Universidad.EClases.Laboratorio);
+                        break;
+                    case 1:
+                        this.clasesDelDia.Enqueue(Universidad.EClases.Legislacion);
+                        break;
+                    case 2:
+                        this.clasesDelDia.Enqueue(Universidad.EClases.Programacion);
+                        break;
+                    case 3:
+                        this.clasesDelDia.Enqueue(Universidad.EClases.SPD);
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Huallpa.Wilson.2D.TP3/Archivos/Clases Instanciables/Universidad.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Archivos;
+using Excepciones;
+
+namespace Clases_Instanciables
+{
+   
+    public class Universidad
+    {
+        #region Enumerados
+        public enum EClases
+        {
+            Programacion,
+            Laboratorio,
+            Legislacion,
+            SPD
+        }
+        #endregion
+
+        #region Atributos
+        List<Alumno> alumnos;
+        List<Jornada> jornada;
+        List<Profesor> profesores;
+        #endregion
+
+        #region Propiedades
+
+        /// <summary>
+        /// Propiedad Alumnos
+        /// </summary>
+        public List<Alumno> Alumnos
+        {
+            get { return this.alumnos; }
+            set { this.alumnos = value; }
+        }
+        /// <summary>
+        /// Propiedad Profesores
+        /// </summary>
+        public List<Profesor> Instructores
+        {
+            get { return this.profesores; }
+            set { this.profesores = value; }
+        }
+        /// <summary>
+        /// Propiedad Jornadas
+        /// </summary>
+        public List<Jornada> Jornada
+        {
+            get { return this.jornada; }
+            set { this.jornada = value; }
+
+        }
+        /// <summary>
+        /// Indexador jornada
+        /// </summary>
+        /// <param name="i"></param>
+        /// <returns></returns>
+        public Jornada this[int i]
+        {
+            get
+            {
+                if (i >= 0 && i < this.jornada.Count)
+                {
+                    return this.jornada[i];
+                }
+                else
+                {
+                    throw new ArchivosException("Indice invalido");
+                }
+            }
+            set
+            {
+                if (i >= 0 && i < this.jornada.Count)
+                {
+                    this.jornada[i] = value;
+                }
+                else
+                {
+                    throw new ArchivosException("Indice invalido");
+                }
+            }
+        }
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public Universidad()
+        {
+            alumnos = new List<Alumno>();
+            jornada = new List<Jornada>();
+            profesores = new List<Profesor>();
+        }
+        #endregion
+
+        #region Metodos
+        /// <summary>
+        /// Gestiona el archivo de un objeto de tipo Universidad en formato xml
+        /// </summary>
+        /// <param name="uni"></param>
+        /// <returns>true si se leyo correctamente, false caso contrario</returns>
+        public static bool Guardar(Universidad uni)
+        {
+            string path = String.Concat(AppDomain.CurrentDomain.BaseDirectory, "universidad.xml");
+            Xml<Universidad> auxUni = new Xml<Universidad>();
+
+            return auxUni.Guardar(path, uni);
+        }
+        /// <summary>
+        /// Gestiona la lectura de objetos de tipo Universidad en formato xml
+        /// </summary>
+        /// <returns>Objeto de tipo Universidad</returns>
+        public static Universidad Leer()
+        {
+            Universidad datos = new Universidad();
+            string path = String.Concat(AppDomain.CurrentDomain.BaseDirectory, "universidad.xml");
+            Xml<Universidad> auxUni = new Xml<Universidad>();
+
+            auxUni.Leer(path, out datos);
+
+            return datos;
+        }
+
+        /// <summary>
+        /// Muestra todos los datos del objeto de tipo Universidad recibido por parametro
+        /// </summary>
+        /// <param name="uni"></param>
+        /// <returns>una cadena con todos los datos del objeto de tipo Universidad recibido por parametro</returns>
+        private string MostrarDatos(Universidad uni)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var item in uni.jornada)
+            {
+                sb.AppendLine(item.ToString());
+            }
+
+            return sb.ToString();
+        }
+        /// <summary>
+        /// Hace publicos los datos del metodo de clase MostrarDatos
+        /// </summary>
+        /// <returns>todos los datos del objeto de tipo Universidad</returns>
+        public override string ToString()
+        {
+            return MostrarDatos(this);
+        }
+        #endregion
+
+        #region Operadores
+      
+        /// <summary>
+        /// Un Universidad será igual a un Alumno si el mismo está inscripto en él.
+        /// </summary>
+        /// <param name="g"></param>
+        /// <param name="a"></param>
+        /// <returns>true si la validacion es correcta, caso contrario devuelve false</returns>
+        public static bool operator ==(Universidad g, Alumno a)
+        {
+            foreach (Alumno item in g.alumnos)
+            {
+                if (item == a)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        /// <summary>
+        /// Un Universidad será distinto a un Alumno si el mismo no está inscripto en él.
+        /// </summary>
+        /// <param name="g"></param>
+        /// <param name="a"></param>
+        /// <returns>true si la validacion es correcta, caso contrario devuelve false</returns>
+        public static bool operator !=(Universidad g, Alumno a)
+        {
+            return !(g == a);
+        }
+        /// <summary>
+        /// Un Universidad será igual a un Profesor si el mismo está dando clases en él.
+        /// </summary>
+        /// <param name="g"></param>
+        /// <param name="i"></param>
+        /// <returns>true si la validacion es correcta, caso contrario devuelve false</returns>
+        public static bool operator ==(Universidad g, Profesor p)
+        {
+            foreach (var item in g.jornada)
+            {
+                if (item.Instructor == p)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        /// <summary>
+        /// Un Universidad será distinto a un Profesor si el mismo no está dando clases en él.
+        /// </summary>
+        /// <param name="g"></param>
+        /// <param name="i"></param>
+        /// <returns>true si la validacion es correcta, caso contrario devuelve false</returns>
+        public static bool operator !=(Universidad g, Profesor i)
+        {
+            return !(g == i);
+        }
+        /// <summary>
+        /// La igualación entre un Universidad y una Clase retornará el primer Profesor capaz de dar esa clase.
+        /// </summary>
+        /// <param name="u"></param>
+        /// <param name="clase"></param>
+        /// <returns>retornará el primer Profesor capaz de dar esa clase. Sino, lanzará la Excepción SinProfesorException.</returns>
+        public static Profesor operator ==(Universidad g, EClases clase)
+        {
+            foreach (var item in g.profesores)
+            {
+                if (item == clase)
+                {
+                    return item;
+                }
+            }
+
+            throw new SinProfesorException("No hay profesor disponible para la clase requerida");
+        }
+        /// <summary>
+        /// retornará el primer Profesor que no pueda dar la clase.
+        /// </summary>
+        /// <param name="u"></param>
+        /// <param name="clase"></param>
+        /// <returns>retornará el primer Profesor que no pueda dar la clase. si no lo encuentra retorna null</returns>
+        public static Profesor operator !=(Universidad u, Universidad.EClases clase)
+        {
+            foreach (var item in u.profesores)
+            {
+                if (item != clase)
+                {
+                    return item;
+                }
+            }
+
+            throw new SinProfesorException();
+        }
+        /// <summary>
+        /// Agrega un alumno a la lista verificando que no esté previamente cargado
+        /// </summary>
+        /// <param name="u"></param>
+        /// <param name="a"></param>
+        /// <returns>devuelve la lista con el alumno agregado, si ya estaba cargado previamente lanza la excepcion AlumnoRepetidoException</returns>
+        public static Universidad operator +(Universidad u, Alumno a)
+        {
+            if (u != a)
+            {
+                u.alumnos.Add(a);
+
+            }
+            else
+            {
+                throw new AlumnoRepetidoException("El alumno que intenta cargar, ya fue agregado a la lista previamente");
+            }
+
+            return u;
+        }
+        /// <summary>
+        /// Agrega un profesor a la lista verificando que no esté previamente cargado
+        /// </summary>
+        /// <param name="u"></param>
+        /// <param name="i"></param>
+        /// <returns>devuelve la lista con el profesor agregado (si corresponde realizar la carga)</returns>
+        public static Universidad operator +(Universidad u, Profesor i)
+        {
+            if (u != i)
+            {
+
+                u.profesores.Add(i);
+            }
+
+            return u;
+        }
+        /// <summary>
+        /// genera y agrega una nueva Jornada indicando la clase, un Profesor que pueda darla y la lista de alumnos que la toman.
+        /// </summary>
+        /// <param name="g"></param>
+        /// <param name="clase"></param>
+        /// <returns>El objeto de tipo Universidad con la Jornada cargada</returns>
+        public static Universidad operator + (Universidad g, EClases clase)
+        {
+            Jornada auxJornada = new Jornada(clase, (g == clase));
+
+            foreach (Alumno item in g.alumnos)
+            {
+                if (item == clase)
+                {
+                    auxJornada += item;
+                }
+            }
+
+            g.jornada.Add(auxJornada);
+
+            return g;
+        }
+        #endregion
+    }
+}
